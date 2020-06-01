b351fb8b1c440e702f9ee724c5897b40a1809110
SammySupreme B <samb9382@gmail.com>
2020-06-01 14:45:34 -0500

    instantiated 6 different value nodes

--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -10,6 +10,13 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
+            BinaryTree binaryTree = new BinaryTree();
+            binaryTree.Add(80);
+            binaryTree.Add(55);
+            binaryTree.Add(69);
+            binaryTree.Add(77);
+            binaryTree.Add(8);
+            binaryTree.Add(32);
         }
     }
 }
